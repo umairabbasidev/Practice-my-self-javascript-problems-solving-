962903562b7da6249e78593bcccd560ebd04d0fb
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-11-16 19:05:48 +0500

    completed chapter two problem solving  questions soloution

--- Chapter1-5.js
+++ Chapter1-5.js
@@ -1,6 +1,6 @@
 // Chapter 1 to 5 problems soloution 
 
-// chapter 1
+// chapter 1(alert)
 
 // Q1 
 // alert("Hello Greetings")
@@ -22,6 +22,52 @@
 
 // Q7 
 // alert("Hello World")
+
+// __________X________X_________
+
+// chapeter 2(VARIABLES FOR STRINGS)
+
+// Q1 
+let username;
+
+// Q2 
+let myName = "Muhammad Umair Abbasi";
+
+// Q3 
+let message;
+message = "Hello World";
+// alert(message)
+
+// Q4 
+let studentName = "Umair";
+let studentAge = "21 years old";
+let studentCourseName = "Certified Mobile Application Development ";
+let completeInformation = studentName + "\n" +  studentAge + "\n"  + studentCourseName
+// alert(completeInformation)
+
+// Q5 
+// let food = "PIZZA"
+// alert(food + "\n" + )
+
+// Q6
+let email = "example123@gmail.com";
+// alert(`My email address is ${email}`)
+
+// Q7 
+let book = "A smarter way to learn JavaScript"
+// alert(`I am trying to learn from the Book \n ${book}`)
+
+// Q8 
+// document.write("<b>Yah! I can write HTMLcontent through javascript </b> ")
+
+
+
+// Q9 
+let myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(myString)
+// console.log(myString)
+// document.write(myString)
+
 
 
 
