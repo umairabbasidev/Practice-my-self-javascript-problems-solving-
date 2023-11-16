049723c5e598bf23df6abefc6a2a71e1dfa0b05a
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-11-16 18:28:57 +0500

    completed chapter one problem solving  questions soloution

---
+++ Chapter1-5.js
@@ -0,0 +1,28 @@
+// Chapter 1 to 5 problems soloution 
+
+// chapter 1
+
+// Q1 
+// alert("Hello Greetings")
+
+// Q2 
+// alert("Error! Please enter a valid password");
+
+// Q3 
+// alert("Welcome to JS Land\nHappy Coding!")
+
+// Q4 
+// alert("Welcome to JS Land")
+
+// Q5 
+// alert("Happy Coding!")
+
+// Q6 
+// console.log("Hello... I Can run  Js through my web browser console");
+
+// Q7 
+// alert("Hello World")
+
+
+
+
