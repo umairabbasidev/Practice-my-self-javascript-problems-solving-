745ac8327d25930aca6614819bad1a7417357424
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-11-17 20:47:50 +0500

    completed chapter 4 and halft chapter 5

--- Chapter1-5.js
+++ Chapter1-5.js
@@ -94,5 +94,59 @@ let productName = "T-shirts"
 
 // document.write(`<b>${userName}</b> ordered <b>${noOFOrders}</b> <b>${productName}</b> on <b>${websiteName}</b>`)
 
+// ____X____X_______
 
+// chaper 4('VARIABLE NAMES: LEGAL & ILLEGAL')
+
+// Q1 
+let firstName = "Umair",lastName = "Abbasi",myAge=21;
+
+// Q2 
+// let legalVariableName;
+
+// document.write(`<b>Rules for naming js variables</b> </br> </br> Variable names can only contain number,$,and_. For example $my_1stVariable Variables must begin with a letter ,$._ For example $name, _name or name Variable names are case Variable names should not be JS keyword`)
+
+// ________X________X_________
+
+
+
+// chaper 5('MATH EXPRESSIONS')
+
+// Q1 
+
+// let num1 = +prompt("Enter the first number")
+// let num2 = +prompt("Enter the second number")
+// let addNum = (num1+num2)
+
+
+// Q2
+// let addNum = (num1-num2)
+// let addNum = (num1*num2)
+// let addNum = (num1/num2)
+// let addNum = (num1%num2)
+// document.write(`The divison of ${num1} + ${num2} is: ${addNum}<br>`)
+// document.write(`The divison of ${num1} - ${num2} is: ${addNum}<br>`)
+// document.write(`The divison of ${num1} * ${num2} is: ${addNum}<br>`)
+// document.write(`The divison of ${num1} / ${num2} is: ${addNum}<br>`)
+// document.write(`The divison of ${num1} % ${num2} is: ${addNum}<br>`)
+
+// Q3 
+// let myVar;
+// document.write(`value after variable declaration is: <b>${typeof myVar}</b><br>`)
+// myVar = 5;
+// document.write(`Initial value is:${myVar}<br>`)
+// myVar++;
+// document.write(`Value after increment is: ${myVar}<br>`)
+// myVar+=7
+// document.write(`Value after addition is: ${myVar}<br>`)
+// myVar--
+// document.write(`Value after decrement is: ${myVar}<br>`)
+// myVar%=3;
+// document.write(`The remainder is ${myVar}`)
+
+// Q4 
+let ticket = 600
+let buyingTickets =5;
+document.write(`Total cost to buy  ${buyingTickets} tickets  is: ${ticket*buyingTickets} PKR`)
+// document.write("Total price of " + buyingTickets + "is" + buyingTickets*ticket)
 
